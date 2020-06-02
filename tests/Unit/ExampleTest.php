73e89b03a0d439a4b3b66ff4899b67f0dc5b2fa8
Balázs Váradi <netpok@gmail.com>
2020-06-02 10:38:01 +0200

    Add more tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,9 +11,10 @@ class ExampleTest extends TestCase
 {
     public function testCastIsNotSingleton()
     {
-        $user                = new UnsavedModel();
-        $now                 = (string) now();
+        $user = new UnsavedModel();
+        $now = (string) now();
         $user->not_singleton = $now;
+        $user->save();
 
         $this->assertEquals($now, (string) $user->not_singleton);
 
@@ -22,12 +23,57 @@ class ExampleTest extends TestCase
 
         $this->assertEquals($now, (string) $user->not_singleton);
     }
+
+    public function testArrayIsNotSingleton()
+    {
+        $user = new UnsavedModel();
+        $user->array = ['a' => 'foo'];
+
+        $this->assertEquals(['a' => 'foo'], $user->array);
+
+        $notSingleton = $user->array;
+        $notSingleton['a'] = 'bar';
+
+        $this->assertEquals(['a' => 'foo'], $user->array);
+    }
+
+    public function testCollectionIsNotSingleton()
+    {
+        $user = new UnsavedModel();
+        $user->collection = ['a' => 'foo'];
+
+        $this->assertEquals('foo', $user->collection->get('a'));
+
+        $notSingleton = $user->collection;
+        $notSingleton->put('a', 'bar');
+
+        $this->assertEquals('foo', $user->collection->get('a'));
+        $this->assertEquals('bar', $notSingleton->get('a'));
+    }
+
+    public function testCreatedAtIsNotSingleton()
+    {
+        $user = new UnsavedModel();
+        $now = (string) now();
+        $user->created_at = $now;
+
+        $this->assertEquals($now, (string) $user->created_at);
+
+        $notSingleton = $user->created_at;
+        $notSingleton->addDays(7);
+
+        $this->assertEquals($now, (string) $user->created_at);
+    }
 }
 
 class UnsavedModel extends Model
 {
+    protected $dateFormat = Carbon::ISO8601;
+
     protected $casts = [
         'not_singleton' => DontSingleton::class,
+        'array' => 'array',
+        'collection' => 'collection',
     ];
 }
 
